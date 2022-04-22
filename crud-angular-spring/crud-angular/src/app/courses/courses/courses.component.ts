829f115a27b20e4fb0103b6aa29ec76d0c3a631d
Edvaldo Junior <edvaldo1jr@gmail.com>
2022-04-22 00:17:42 -0300

    Inclusao de chamada HTTP Get Angular e RXJS, tratamento de Erros e MatDialog

--- crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,9 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -9,17 +12,34 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses$: Observable<Course[]>;
 //    {_id: "1", name: 'Angular', category: 'front-end'}];
   displayedColumns = ['name', 'category'];
 
   // courseService: CourseService;
-  constructor(private coursesService: CoursesService) { }
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
   // this.courses = [];
   // this.coursesService = new CourseService();
+  this.courses$ = this.coursesService.list()
+  .pipe(
+    catchError(error => {
+      this.onError('Erro ao carregar os cursos.');
+      return of([])
+    })
+  );
+  }
+
+  onError(errorMsg: string){
+      this.dialog.open(ErrorDialogComponent, {
+        data: errorMsg
+      });
+  }
 
   ngOnInit(): void {
-    this.courses = this.coursesService.list();
+
   }
 
 }
