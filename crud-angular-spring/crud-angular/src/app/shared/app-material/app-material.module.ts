d21095149b9e683bb19e08e4528c8f9e2abb5bff
Edvaldo Junior <edvaldo1jr@gmail.com>
2022-05-24 21:14:51 -0300

    Utilizando o Pipe para mostrar ícone

--- crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -5,11 +5,12 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @NgModule({
-exports: [MatCardModule, MatTableModule, MatToolbarModule, MatProgressSpinnerModule, MatDialogModule, MatButtonModule]
+exports: [MatCardModule, MatTableModule, MatToolbarModule, MatProgressSpinnerModule, MatDialogModule, MatButtonModule, MatIconModule]
 
 })
 export class AppMaterialModule { }
